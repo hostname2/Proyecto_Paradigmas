7efea045655e54aac3db794955d543d6b355f51b
Sebastian Grisanti <sgrisanti97@gmail.com>
2020-09-16 20:21:48 -0600

    Metodo post js

---
+++ js/getJSON.js
@@ -0,0 +1,10 @@
+
+
+function getJSON(url, data, callback) {
+    fetch(url, {
+        method: 'POST',
+        body: data
+    }).then((result) => {
+        return result.json();
+    }).then(callback);
+}
